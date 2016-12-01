de0ffd662ff2d431558259df410fced0456825c3
calint <calin.tenitchi@gmail.com>
2016-12-01 13:15:33 +0700

    .

--- src/glos.c
+++ src/glos.c
@@ -1,5 +1,39 @@
 #include "SDL.h"
 
+static SDL_Rect sprites_pos[2];
+static SDL_Surface*sprites_image[2];
+
+static inline void sprites_init() {
+	SDL_Surface* temp = SDL_LoadBMP("logo.bmp");
+	SDL_Surface* bg = SDL_DisplayFormat(temp);
+	SDL_FreeSurface(temp);
+
+	sprites_image[0]=bg;
+	sprites_image[1]=bg;
+
+	sprites_pos[0].x=100;
+	sprites_pos[0].y=100;
+	sprites_pos[0].w=74;
+	sprites_pos[0].h=55;
+
+	sprites_pos[1].x=200;
+	sprites_pos[1].y=100;
+	sprites_pos[1].w=74;
+	sprites_pos[1].h=55;
+}
+
+static inline void sprites_free() {
+	for (int i = 0; i < sizeof sprites_pos; i++) {
+		SDL_FreeSurface(sprites_image[i]);
+	}
+}
+
+static inline void sprites_render(SDL_Surface*s) {
+	for (int i = 0; i < sizeof sprites_pos; i++) {
+		SDL_BlitSurface(sprites_image[i], NULL, s, &sprites_pos[i]);
+	}
+}
+
 int main(int argc, char *argv[]) {
 	/* initialize SDL */
 	SDL_Init(SDL_INIT_VIDEO);
@@ -11,22 +45,11 @@ int main(int argc, char *argv[]) {
 	SDL_Surface* screen = SDL_SetVideoMode(640, 480, 0, 0);
 
 	/* load bitmap to temp surface */
-	SDL_Surface* temp = SDL_LoadBMP("logo.bmp");
-
-	/* convert bitmap to display format */
-	SDL_Surface* bg = SDL_DisplayFormat(temp);
-
-	/* free the temp surface */
-	SDL_FreeSurface(temp);
 
 	SDL_Event event;
 	int gameover = 0;
 
-	   SDL_Rect destination;
-	   destination.x = 100;
-	   destination.y = 100;
-	   destination.w = 65;
-	   destination.h = 44;
+	sprites_init();
 
 	/* message pump */
 	while (!gameover) {
@@ -51,15 +74,13 @@ int main(int argc, char *argv[]) {
 			}
 		}
 
-		/* draw the background */
-		SDL_BlitSurface(bg, NULL, screen, &destination);
+		sprites_render(screen);
 
 		/* update the screen */
 		SDL_UpdateRect(screen, 0, 0, 0, 0);
 	}
 
-	/* free the background surface */
-	SDL_FreeSurface(bg);
+	sprites_free();
 
 	/* cleanup SDL */
 	SDL_Quit();
