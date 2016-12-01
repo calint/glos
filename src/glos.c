2b9f8dee7d7e4ffe821a0c9c4e4a3e7c513b500a
calint <calin.tenitchi@gmail.com>
2016-12-01 14:26:12 +0700

    .

--- src/glos.c
+++ src/glos.c
@@ -1,13 +1,6 @@
 #include "SDL.h"
-#include <stdlib.h>
 
-#define screen_width 512
-#define screen_height 512
-
-#define sprites_count 1024
-static SDL_Rect sprites_pos[sprites_count];
-static SDL_Surface*sprites_image[sprites_count];
-
+//------------------------------------------------------------------ library
 static inline float random() {
 	return rand() / (float) RAND_MAX;
 }
@@ -16,61 +9,127 @@ static inline int randomRange(int low, int high) {
 	int range = high - low;
 	return (int) (random() * range) + low;
 }
+//------------------------------------------------------------------ screen
+#define screen_width 512
+#define screen_height 512
+static SDL_Window *win;
+static SDL_Renderer *ren;
+static inline void screen_init() {
+
+	win = SDL_CreateWindow("Hello World!", 100, 100, screen_width,
+	screen_height, SDL_WINDOW_SHOWN);
+	if (!win) {
+		SDL_Quit();
+		exit(2);
+	}
+
+	ren = SDL_CreateRenderer(win, -1,
+			SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (!ren) {
+		SDL_DestroyWindow(win);
+		SDL_Quit();
+		exit(3);
+	}
+
+}
+
+static inline void screen_free() {
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
+}
+
+//------------------------------------------------------------------ surfaces
+#define texture_count 1
+static SDL_Texture *texture[texture_count];
+
+static inline void textures_init() {
+	SDL_Surface *bmp = SDL_LoadBMP("logo.bmp");
+	if (!bmp) {
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		SDL_Quit();
+		exit(4);
+	}
+
+	SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
+	SDL_FreeSurface(bmp);
+	if (!tex) {
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		SDL_Quit();
+		exit(5);
+	}
+
+	texture[0] = tex;
+}
+
+static inline void texture_free() {
+	for (int i = 0; i < texture_count; i++) {
+		SDL_DestroyTexture(texture[i]);
+	}
+}
+
+//------------------------------------------------------------------ sprites
+#define sprite_count 1024
+struct {
+	float x, y, dx, dy;
+	int w, h;
+	SDL_Texture *img;
+} sprite[sprite_count];
 
 static inline void sprites_init() {
-	SDL_Surface* temp = SDL_LoadBMP("logo.bmp");
-	SDL_Surface* bg = SDL_DisplayFormat(temp);
-	SDL_FreeSurface(temp);
+	for (int i = 0; i < sprite_count; i++) {
+		sprite[i].img = texture[0];
+		sprite[i].x = randomRange(0, screen_width);
+		sprite[i].y = randomRange(0, screen_height);
+		sprite[i].w = 74;
+		sprite[i].h = 55;
 
-	for (int i = 0; i < sprites_count; i++) {
-		sprites_image[i] = bg;
-		sprites_pos[i].x = randomRange(0, screen_width);
-		sprites_pos[i].y = randomRange(0, screen_height);
-		sprites_pos[i].w = 74;
-		sprites_pos[i].h = 55;
+		sprite[i].dx = randomRange(-5, 5);
+		sprite[i].dy = randomRange(-5, 5);
 	}
 }
 
-static inline void sprites_free() {
-	for (int i = 0; i < sprites_count; i++) {
-		SDL_FreeSurface(sprites_image[i]);
-	}
+static inline void sprites_update() {
 }
 
-static inline void sprites_render(SDL_Surface*s) {
-	for (int i = 0; i < sprites_count; i++) {
-		SDL_BlitSurface(sprites_image[i], NULL, s, &sprites_pos[i]);
+static inline void sprites_render(SDL_Renderer*r) {
+	SDL_RenderClear(r);
+
+	for (int i = 0; i < sprite_count; i++) {
+		SDL_RenderCopy(r, sprite[0].img, NULL, NULL);
 	}
+
+	SDL_RenderPresent(ren);
 }
+
+//--------------------------------------------------------------------- main
 
 void print_SDL_version(char* preamble, SDL_version* v) {
 	printf("%s %u.%u.%u\n", preamble, v->major, v->minor, v->patch);
 }
 
 void print_SDL_versions() {
+
 	SDL_version ver;
 
-// Prints the compile time version
 	SDL_VERSION(&ver);
+
 	print_SDL_version("SDL compile-time version", &ver);
 
-// Prints the run-time version
-	ver = *SDL_Linked_Version();
-	print_SDL_version("SDL runtime version", &ver);
 }
 
 int main(int argc, char *argv[]) {
 	print_SDL_versions();
 
-	SDL_Init(SDL_INIT_VIDEO);
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		SDL_Quit();
+		return 1;
+	}
 
-	SDL_WM_SetCaption("SDL Test", "SDL Test");
+	screen_init();
 
-	SDL_Surface* screen = SDL_SetVideoMode(screen_width, screen_height, 0, 0);
-
-	SDL_Event event;
-
-	int gameover = 0;
+	textures_init();
 
 	sprites_init();
 
@@ -81,6 +140,10 @@ int main(int argc, char *argv[]) {
 	Uint32 calculate_fps_every_x_ms = 1000;
 
 	Uint32 t0 = SDL_GetTicks();
+
+	SDL_Event event;
+
+	int gameover = 0;
 
 	while (!gameover) {
 
@@ -105,9 +168,7 @@ int main(int argc, char *argv[]) {
 			}
 		}
 
-		sprites_render(screen);
-
-		SDL_UpdateRect(screen, 0, 0, 0, 0);
+		sprites_render(ren);
 
 		Uint32 dt = SDL_GetTicks() - t0;
 
@@ -125,9 +186,8 @@ int main(int argc, char *argv[]) {
 		t0 = SDL_GetTicks();
 	}
 
-	sprites_free();
+	screen_free();
 
-	/* cleanup SDL */
 	SDL_Quit();
 
 	return 0;
