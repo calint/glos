25af2efd28c26b1e562370b4fc2e4e47c0365ed6
calint <calin.tenitchi@gmail.com>
2016-12-02 11:24:43 +0700

    .

--- src/objects.h
+++ src/objects.h
@@ -6,21 +6,25 @@
 
 //-------------------------------------------------------------------- objects
 
-#define objects_count 1024
+#define object_count 1024
 
-static object objects[objects_count];
+static object objects[object_count];
 
-static object*objects_start_ptr = objects;
+static object*objects_start_ptr=objects;
 
-static object*objects_ptr = objects;
+static object*objects_ptr=objects;
 
-static bits objects_bits[objects_count];
+static object*objects_end_ptr=objects+object_count;
 
-static bits*objects_bits_start_ptr = objects_bits;
 
-static bits*objects_bits_ptr = objects_bits;
+static bits objects_bits[object_count];
 
-static bits*objects_bits_end_ptr = objects_bits + objects_count;
+static bits*objects_bits_start_ptr=objects_bits;
+
+static bits*objects_bits_ptr=objects_bits;
+
+static bits*objects_bits_end_ptr=objects_bits+object_count;
+
 
 #define object_bit_allocated 0
 
@@ -69,7 +73,7 @@ inline static void objects_init(){}
 
 inline static void objects_free() {
 	object*o = objects;
-	int i=objects_count;
+	int i=object_count;
 	while(i--){
 		if(o->bits&&bits_is_set(o->bits,object_bit_allocated))
 			if(o->free)
@@ -88,15 +92,9 @@ inline static void add_dt(vec4*this,vec4*other,dt dt){
 
 inline static void objects_update(dt dt){
 	object*o=objects;
-	int i=objects_count;
-	while(i--){
+	while(o<objects_end_ptr){
 		add_dt(&o->position,&o->velocity,dt);
-//		o->position.x+=o->velocity.x*dt;
-//		o->position.y+=o->velocity.y*dt;
-//		o->position.z+=o->velocity.z*dt;
-		o->angle.x+=o->angular_velocity.x*dt;
-		o->angle.y+=o->angular_velocity.y*dt;
-		o->angle.x+=o->angular_velocity.z* dt;
+		add_dt(&o->angle,&o->angular_velocity,dt);
 		if (o->update)
 			o->update(o, dt);
 		o++;
@@ -106,7 +104,7 @@ inline static void objects_update(dt dt){
 
 inline static void objects_render() {
 	object*o = objects;
-	int i = objects_count;
+	int i = object_count;
 	while (i--) {
 		if (o->render)
 			o->render(o);
