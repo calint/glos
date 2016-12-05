e5e1ed60fb3d32f3a89959938f98def05e906c4f
calint <calin.tenitchi@gmail.com>
2016-12-06 02:58:53 +0700

    .

--- src/obj_file.h
+++ src/obj_file.h
@@ -1,12 +1,4 @@
 #pragma once
-#include<unistd.h>
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
-#include<sys/stat.h>
-#include<stdlib.h>
-#include<fcntl.h>
-#include"lib.h"
 #include"token.h"
 
 //# Blender MTL File: 'None'
