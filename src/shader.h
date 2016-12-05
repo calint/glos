68e6ecc214206df3c9a90f401e61c48c0707220d
calint <calin.tenitchi@gmail.com>
2016-12-05 16:06:31 +0700

    .

--- src/shader.h
+++ src/shader.h
@@ -166,35 +166,29 @@ inline static void load_program() {
 	}
 	shader.sampler_slot=(GLuint)slot;
 
-	GLuint textureID;
-
-//	int width=1,height=1;
-//	float pixels[]={0.0f, 0.5f, 0.0f};
-//
+	// texture
 	int width=2,height=2;
 	float pixels[]={
-			1.0f, 0.0f, 0.0f,   0.0f, 0.0f, .5f,
+			1.0f, 1.0f, 0.0f,   0.0f, 0.0f, .5f,
 			0.0f, 0.0f, 1.0f,   1.0f, 1.0f, 0.0f
 	};
-
-//	unsigned char*image =SOIL_load_image("logo.jpg",&width,&height,0,
-//			SOIL_LOAD_RGB);
-
+	GLuint textureID;
 	glGenTextures(1,&textureID);
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D,textureID);
 	glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_FLOAT,pixels);
-//	glGenerateMipmap(GL_TEXTURE_2D);
-	//	SOIL_free_image_data(image);
-//	glBindTexture(GL_TEXTURE_2D,0);
-
 	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+	glGenerateMipmap(GL_TEXTURE_2D);
 
+
+	// render
 	glUniform1i((signed)shader.sampler_slot,0);
 
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D,textureID);
 
 	glEnableVertexAttribArray(shader.position_slot);
 	glEnableVertexAttribArray(shader.color_slot);
