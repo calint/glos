1a7fa83553a09d2a5472a771e57204eb0e6a1ade
calint <calin.tenitchi@gmail.com>
2016-12-06 02:40:27 +0700

    .

--- src/globs.h
+++ src/globs.h
@@ -26,22 +26,22 @@ typedef struct vertex{
 //	{{-.5, .5, 0},{ 0, 0, 1,1},{0,0,1},{0,1}},
 //	{{-.5,-.5, 0},{ 0, 0, 0,1},{0,0,1},{0,0}},
 //};
-static vertex shader_def_vertbuf[]={
+static vertex glob_def_vertbuf[]={
 	{{ .5,-.5, 0},{ 1, 0, 0,1},{0,0,1},{ 1,-1}},
 	{{ .5, .5, 0},{ 0, 1, 0,1},{0,0,1},{ 1, 1}},
 	{{-.5, .5, 0},{ 0, 0, 1,1},{0,0,1},{-1, 1}},
 	{{-.5,-.5, 0},{ 0, 0, 0,1},{0,0,1},{-1,-1}},
 };
-static GLubyte shader_def_ixbuf[]={0,1,2,2,3,0};
+static GLubyte glob_def_ixbuf[]={0,1,2,2,3,0};
 
-#define shader_def_texwi 2
-#define shader_def_texhi 2
-static GLfloat shader_def_texbuf[]={
+#define glob_def_texwi 2
+#define glob_def_texhi 2
+static GLfloat glob_def_texbuf[]={
 		.7f, .7f, .7f,    .2f, .2f, .2f,
 		.2f ,.2f ,.2f,    .7f, .7f, .7f
 };
 
-static float shader_def_mtx_mw[]={
+static float glob_def_mtx_mw[]={
 		1,0,0,0,
 		0,1,0,0,
 		0,0,1,0,
@@ -66,29 +66,29 @@ glob{
 }glob;
 
 static glob glob_def=(glob){
-	.vbufn=sizeof(shader_def_vertbuf)/sizeof(&shader_def_vertbuf[0]),
-	.vbufnbytes=sizeof(shader_def_vertbuf),
-	.ibuf=shader_def_ixbuf,
-	.ibufn=sizeof(shader_def_ixbuf)/sizeof(GLubyte),
-	.ibufnbytes=sizeof(shader_def_ixbuf),
+	.vbufn=sizeof(glob_def_vertbuf)/sizeof(&glob_def_vertbuf[0]),
+	.vbufnbytes=sizeof(glob_def_vertbuf),
+	.ibuf=glob_def_ixbuf,
+	.ibufn=sizeof(glob_def_ixbuf)/sizeof(GLubyte),
+	.ibufnbytes=sizeof(glob_def_ixbuf),
 	.texbufid=0,
 	.mtx_mw=0,
-	.texbuf=shader_def_texbuf,
+	.texbuf=glob_def_texbuf,
 	.texbufid=0,
-	.texwi=shader_def_texwi,
-	.texhi=shader_def_texhi,
-	.mtx_mw=shader_def_mtx_mw,
-	.vbuf=shader_def_vertbuf,
+	.texwi=glob_def_texwi,
+	.texhi=glob_def_texhi,
+	.mtx_mw=glob_def_mtx_mw,
+	.vbuf=glob_def_vertbuf,
 };
 
 static glob globs[globs_cap];
 
-inline static void shader_render_glob(glob*sr){
+inline static void glob_render(glob*this){
 	shader_render_triangle_array(
-			sr->vbufid,
-			sr->vbufn,
-			sr->texbufid,
-			sr->mtx_mw
+			this->vbufid,
+			this->vbufn,
+			this->texbufid,
+			this->mtx_mw
 	);
 }
 
@@ -100,7 +100,7 @@ inline static void globs_render(){
 	glob*g=globs;
 	int n=globs_cap;
 	while(n--){
-		shader_render_glob(g++);
+		glob_render(g++);
 	}
 }
 
