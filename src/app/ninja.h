52e029517d93bff0cdb1df18d586132932c1c4b4
calint <calin.tenitchi@gmail.com>
2016-12-11 09:47:30 +0700

    .

--- src/app/ninja.h
+++ src/app/ninja.h
@@ -5,20 +5,20 @@
 //----------------------------------------------------------------------- init
 static object ninja_def={
 	.node=node_def,
-	.bvol={
-			.bounding_radius=1.4f,
-			.scale={1,1,1,0}
+	.bvol={	.bounding_radius=1.4f,
+			.scale={1,1,1,0}},
+	.phy={	.position=vec4_def,
+			.velocity=vec4_def,
+			.angle=vec4_def,
+			.angular_velocity=vec4_def,},.
+	vtbl={	.init=NULL,
+			.update=NULL,
+			.collision=NULL,
+			.render=object_render_glob,
+			.free=NULL,
 	},
-	.phy=phy_def,
 	.type={{'a',0,0,0,0,0,0,0}},
+	.part={NULL,NULL,NULL,NULL},
 	.ptr_to_bits=NULL,
-	.vtbl={
-		.init=NULL,
-		.update=NULL,
-		.collision=NULL,
-		.render=object_render_glob,
-		.free=NULL,
-	},
-	.part={NULL,NULL,NULL,NULL}
 };
 //----------------------------------------------------------------------------
