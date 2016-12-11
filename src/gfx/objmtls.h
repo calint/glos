a2d74df83d4900b11dfe8d9d8225f501127e9b22
calint <calin.tenitchi@gmail.com>
2016-12-11 18:20:58 +0700

    .

--- src/gfx/objmtls.h
+++ src/gfx/objmtls.h
@@ -1,8 +1,8 @@
 #pragma once
-#include"../lib.h"
+#include<stdio.h>
 //----------------------------------------------------------------------config
 
-#define objmtls_initial_capacity 128
+#define objmtls_initial_capacity 8
 #define objmtls_bounds_check 1
 
 //------------------------------------------------------------------------ def
@@ -54,7 +54,7 @@ inline static void objmtls_add(objmtls*this,objmtl* o){
 
 inline static objmtl* objmtls_get(objmtls*this,unsigned index){
 #ifdef objmtls_bounds_check
-	if(index>=this->cap){
+	if(index>=this->count){
 		fprintf(stderr,"\nindex-out-of-bounds");
 		fprintf(stderr,"\t%s\n\n%d  index: %u    capacity: %u\n",
 				__FILE__,__LINE__,index,this->cap);
@@ -83,19 +83,13 @@ inline static size_t objmtls_size_in_bytes(objmtls*this){
 inline static void objmtls_free(objmtls*this){
 	if(!this->data)
 		return;
-
-	objmtl*it=*this->data;
-	unsigned i=this->count;
-	while(i--)
-		free(it++);
-
 	free(this->data);
 }
 
 //-----------------------------------------------------------------------------
 
-inline static void objmtls_add_list(objmtls*this,/*copies*/const objmtl**str,int n){
-	//? optimize
+inline static void objmtls_add_list(objmtls*this,/*copies*/const objmtl**str,size_t n){
+	//? optimize memcpy
 	const objmtl**p=str;
 	while(n--){
 		_objmtls_insure_free_capcity(this,1);
@@ -124,3 +118,46 @@ inline static void objmtls_write_to_fd(objmtls*this,int fd){
 
 //-----------------------------------------------------------------------------
 
+inline static objmtls objmtls_from_file(const char*path){
+	FILE*f=fopen(path,"rb");
+	if(!f){
+		perror("\ncannot open");
+		fprintf(stderr,"\t%s\n\n%s %d\n",path,__FILE__,__LINE__);
+		exit(-1);
+	}
+	long sk=fseek(f,0,SEEK_END);
+	if(sk<0){
+		fprintf(stderr,"\nwhile fseek\n");
+		fprintf(stderr,"\t\n%s %d\n",__FILE__,__LINE__);
+		exit(-1);
+	}
+	long length=ftell(f);
+	if(length<0){
+		fprintf(stderr,"\nwhile ftell\n");
+		fprintf(stderr,"\t\n%s %d\n",__FILE__,__LINE__);
+		exit(-1);
+	}
+	rewind(f);
+	objmtl**filedata=(objmtl**)malloc((size_t)length+1);
+	if(!filedata){
+		fprintf(stderr,"\nout-of-memory\n");
+		fprintf(stderr,"\t\n%s %d\n",__FILE__,__LINE__);
+		exit(-1);
+	}
+	size_t n=fread(filedata,1,(size_t)length+1,f);
+	if(n!=(size_t)length){
+		fprintf(stderr,"\nnot-a-full-read\n");
+		fprintf(stderr,"\t\n%s %d\n",__FILE__,__LINE__);
+		exit(-1);
+	}
+	fclose(f);
+	filedata[length]=0;
+
+	return (objmtls){
+		.data=filedata,
+		.count=((unsigned)length+1)/sizeof(objmtl*),
+		.cap=(unsigned)length+1
+	};
+}
+
+//-----------------------------------------------------------------------------
--- templates/make_src.sh
+++ templates/make_src.sh
@@ -1,9 +1,13 @@
 #/bin/sh -x
-sh template.sh dyn dynp void* &&
-sh template.sh dyn dynf float &&
-sh template.sh dyn str char &&
-sh template.sh dyn dyni int &&
+./template dyn dynp void* &&
+./template dyn dynf float &&
+./template dyn str char &&
+./template dyn dyni int &&
 mv -f *.h ../src/lib/ &&
-ls -la --color ../src/lib/*.h
+#ls -la --color ../src/lib/*.h
 
+./template dyn objmtls objmtl* &&
+mv -f *.h ../src/gfx/ &&
+
+echo done
 
