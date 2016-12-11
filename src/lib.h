0a839958d6ee6d112729f96dd675cc171c4ec9e3
calint <calin.tenitchi@gmail.com>
2016-12-11 18:37:59 +0700

    .

--- src/lib.h
+++ src/lib.h
@@ -3,33 +3,33 @@
 #include<inttypes.h>
 
 #include"lib/consts.h"
-
-typedef uint8_t bool;
-typedef uint8_t bits;
-typedef uint32_t gid;
-typedef void*ref;
-typedef struct type{char path[8];}type;
-#define type_def (type){{0,0,0,0,0,0,0,0}}
-
-typedef struct vec2{float x,y;}vec2;
-typedef struct vec3{float x,y,z;}vec3;
-typedef struct vec4{float x,y,z,w;}vec4;
-#define vec4_def (vec4){0,0,0,0}
-
-//typedef float vec4[4];
-typedef float mat4[16];
-//struct mat4{float c[4*4];}
-
-typedef vec4 position;
-typedef position point;
-typedef vec4 velocity;
-typedef vec4 angle;
-typedef vec4 scale;
-typedef vec4 angular_velocity;
-typedef float bounding_radius;
-typedef unsigned id;
-typedef float dt;
-typedef const char*path;
+#include"lib/types.h"
+//typedef uint8_t bool;
+//typedef uint8_t bits;
+//typedef uint32_t gid;
+//typedef void*ref;
+//typedef struct type{char path[8];}type;
+//#define type_def (type){{0,0,0,0,0,0,0,0}}
+//
+//typedef struct vec2{float x,y;}vec2;
+//typedef struct vec3{float x,y,z;}vec3;
+//typedef struct vec4{float x,y,z,w;}vec4;
+//#define vec4_def (vec4){0,0,0,0}
+//
+////typedef float vec4[4];
+//typedef float mat4[16];
+////struct mat4{float c[4*4];}
+//
+//typedef vec4 position;
+//typedef position point;
+//typedef vec4 velocity;
+//typedef vec4 angle;
+//typedef vec4 scale;
+//typedef vec4 angular_velocity;
+//typedef float bounding_radius;
+//typedef unsigned id;
+//typedef float dt;
+//typedef const char*path;
 
 #include"lib/str.h"
 #include"lib/dynf.h"
