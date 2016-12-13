6a4eb34fe8354ef24fb2349ed1a5e1fc765b47e8
calint <calin.tenitchi@gmail.com>
2016-12-13 12:34:08 +0700

    .

--- src/net/netc.h
+++ src/net/netc.h
@@ -9,22 +9,31 @@ struct{
 	float pointer_angle_about_x;
 }netc,netc_prv;
 
-
 static netcs[netc_cap];
 
+static unsigned netc_active_player_index;
 
 inline static void netc_init(){
+}
+
+inline static void netc_connect(){
 
 }
 
 inline static void netc_send(){
-
+	netc_prv=netc;
+	// send
 }
 
 inline static void netc_recv(){
 
 }
 
+inline static void netc_disconnect(){
+
+}
+
 inline static void netc_free(){
+	// close network if active
 
 }
