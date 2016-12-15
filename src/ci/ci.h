21c4e681c8df12ab972d650e4117ccb87307cc06
calint <calin.tenitchi@gmail.com>
2016-12-16 04:03:56 +0700

    .

--- src/ci/ci.h
+++ src/ci/ci.h
@@ -90,82 +90,47 @@ static void ci_parse_field(const char**pp,ci_toc*tc,ci_class*c,
 		token*type,token*name){
 	ci_field*f=malloc(sizeof(ci_field));
 	*f=ci_field_def;
-	dynp_add(&c->fields,f);
 	token_copy_to_str(type,&f->type);
 	if(token_is_empty(name)){
 		token_copy_to_str(type,&f->name);
 	}else{
 		token_copy_to_str(name,&f->name);
 	}
-	ci_toc_add_identifier(tc,f->name.data);
-
+	dynp_add(&c->fields,f);
+//	ci_toc_add_identifier(tc,f->name.data);
 }
-static void ci_compile(const char*path){
-	str s=str_from_file(path);
-	const char*p=s.data;
-	ci_toc toc=ci_toc_def;
-	while(1){
-		token t=token_next(&p);
-		if(token_is_empty(&t))
-			break;
-		if(!token_equals(&t,"class")){
-			printf("expected class declaration\n");
-			exit(1);
-		}
-		token nm=token_next(&p);
-		ci_class*c=malloc(sizeof(ci_class));
-		*c=ci_class_def;
-		dynp_add(&ci_classes,c);
-		token_copy_to_str(&nm,&c->name);
 
-		ci_toc_push_scope(&toc, 'c',c->name.data);
-		if(*p==':'){
-			p++;
-			while(1){
-				token extends_name=token_next(&p);
-				dynp_add(&c->extends,/*takes*/token_to_str(&extends_name));
-				if(*p=='{'){
-					break;
-				}else if(*p==','){
-					p++;
-					continue;
-				}
-				printf("<file> <line:col> expected '{' or ',' followed by "
-						"class name");
-				exit(1);
+static void ci_compile_to_c(ci_toc*tc){
+//	for(unsigned i=0;i<ci_classes.count;i++){
+//		ci_class*c=dynp_get(&ci_classes,i);
+//		printf("\n");
+//		printf("//--- - - ---------------------  - -- - - - - - - -- - - - -- - - - -- - - -\n");
+//		printf("static typedef struct %s{\n",c->name.data);
+//		for(unsigned j=0;j<c->extends.count;j++){
+//			str*e=dynp_get(&c->extends,j);
+//			printf("    %s %s;\n",e->data,e->data);
+//		}
+//		for(unsigned j=0;j<c->fields.count;j++){
+//			ci_field*f=dynp_get(&c->fields,j);
+//			printf("    %s %s;\n",f->type.data,f->name.data);
+//		}
+//		printf("}%s;\n",c->name.data);
+//
+//		printf("\n");
+//		// #define object_def {...}
+//		printf("#define %s_def {",c->name.data);
+//		for(unsigned j=0;j<c->extends.count;j++){
+//			str*e=dynp_get(&c->extends,j);
+//			printf("%s_def,",e->data);
+//		}
+//		for(unsigned j=0;j<c->fields.count;j++){
+//			ci_field*f=dynp_get(&c->fields,j);
+//			printf("    %s %s;\n",f->type.data,f->name.data);
+//		}
+//		printf("}\n");
+//	}
+//	return;
 
-			}
-		}
-		// class body starts
-		if(*p=='{'){ // compound
-			p++;
-			while(1){
-				token type=token_next(&p);
-				if(token_is_empty(&type)){
-					if(*p!='}'){
-						printf("<file> <line:col> expected end of class\n");
-						exit(1);
-					}
-					p++;
-					break;
-				}
-				token name=token_next(&p);
-				if(*p=='('){
-					p++;
-					ci_parse_function(&p,&toc,c,&type,&name);
-				}else if(*p=='=' || *p==';'){
-					p++;
-					ci_parse_field(&p,&toc,c,&type,&name);
-				}else{
-					printf("<file> <line:col> expected ';'\n");
-					exit(1);
-				}
-			}
-		}
-		ci_toc_pop_scope(&toc);//, 'c',c->name.data);
-	}
-	// print
-	printf("// compiled from: %s\n",path);
 	dynp_foa(&ci_classes,{
 			ci_class*c=o;
 			printf("\n");
@@ -252,7 +217,7 @@ static void ci_compile(const char*path){
 				printf("){\n");
 				dynp_foa(&f->exprs,{
 					ci_expr*e=o;
-					e->compile(e,&toc);
+					e->compile(e,tc);
 					printf(";\n");
 				});
 				printf("}\n");
@@ -260,5 +225,70 @@ static void ci_compile(const char*path){
 	});
 	printf("//--- - - ---------------------  - -- - - - - - - -- - - - -- - - - -- - - -\n");
 	printf("\n");
+}
+static void ci_compile(const char*path){
+	str s=str_from_file(path);
+	const char*p=s.data;
+	ci_toc toc=ci_toc_def;
+	while(1){
+		token t=token_next(&p);
+		if(token_is_empty(&t))
+			break;
+		if(!token_equals(&t,"class")){
+			printf("expected class declaration\n");
+			exit(1);
+		}
+		token nm=token_next(&p);
+		ci_class*c=malloc(sizeof(ci_class));
+		*c=ci_class_def;
+		dynp_add(&ci_classes,c);
+		token_copy_to_str(&nm,&c->name);
+		ci_toc_push_scope(&toc, 'c',c->name.data);
+		if(*p==':'){
+			p++;
+			while(1){
+				token extends_name=token_next(&p);
+				dynp_add(&c->extends,/*takes*/token_to_str(&extends_name));
+				if(*p=='{'){
+					break;
+				}else if(*p==','){
+					p++;
+					continue;
+				}
+				printf("<file> <line:col> expected '{' or ',' followed by "
+						"class name");
+				exit(1);
+
+			}
+		}
+		// class body starts
+		if(*p=='{'){ // compound
+			p++;
+			while(1){
+				token type=token_next(&p);
+				if(token_is_empty(&type)){
+					if(*p!='}'){
+						printf("<file> <line:col> expected end of class\n");
+						exit(1);
+					}
+					p++;
+					break;
+				}
+				token name=token_next(&p);
+				if(*p=='('){
+					p++;
+					ci_parse_function(&p,&toc,c,&type,&name);
+				}else if(*p=='=' || *p==';'){
+					p++;
+					ci_parse_field(&p,&toc,c,&type,&name);
+				}else{
+					printf("<file> <line:col> expected ';'\n");
+					exit(1);
+				}
+			}
+		}
+		ci_toc_pop_scope(&toc);//, 'c',c->name.data);
+	}
+	ci_compile_to_c(&toc);
 }
 
