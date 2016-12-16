ce972c95fc57e986869c8487b82010d231bdcd21
calint <calin.tenitchi@gmail.com>
2016-12-16 17:40:37 +0700

    .

--- src/ci/expr_bool.h
+++ src/ci/expr_bool.h
@@ -6,15 +6,17 @@
 typedef struct ci_expr_bool{
 	ci_expr super;
 
-	struct ci_expr_bool*lh;
+	struct ci_expr*lh;
 
 	char op;
 
-	struct ci_expr_bool*rh;
+	struct ci_expr*rh;
 
 	str bool_op_list;
 
 	dynp/*owns &expr_bool*/bool_list;
+
+	bool is_encapsulated;
 
 }ci_expr_bool;
 
@@ -30,18 +32,29 @@ inline static void _ci_expr_bool_free_(ci_expr*oo){
 	{str_def,_ci_expr_bool_compile_,_ci_expr_bool_free_},\
 	NULL,0,NULL,\
 	str_def,\
-	dynp_def}
+	dynp_def,\
+	0}
 
-inline static ci_expr*ci_expr_bool_parse(ci_expr_bool*o,
+inline static void ci_expr_bool_parse(ci_expr_bool*o,
 		const char**pp,ci_toc*tc){
 
 	o->super.type=str_from_string("bool");
-	if(**pp!='('){
-
-
+	if(**pp!='('){//   keybits==1 && ok || (a&b!=0)
+		o->is_encapsulated=false;
+		token l=token_next(pp);
+		char op=0;
+		if(**pp=='='){
+			(*pp)++;
+			if(**pp!='='){
+				printf("<file> <line:col> expected '=='\n");
+				exit(1);
+			}
+			op='=';
+		}
 	}
+	o->is_encapsulated=true;
 	(*pp)++;
 	ci_expr_bool*e=malloc(sizeof(ci_expr_bool));
 	*e=ci_expr_bool_def;
-	return(ci_expr*)e;
+	ci_expr_bool_parse(o,pp,tc);
 }
