5fc32da8c71a3037d2adbbf706c27168a2d402f1
calint <calin.tenitchi@gmail.com>
2023-12-20 16:03:13 +0100

    .

--- _archived_/shader.f
+++
@@ -1,9 +0,0 @@
-#version 130
-uniform sampler2D utex;
-in vec4 vrgba;
-in vec3 vnorm;
-in vec2 vtex;
-out vec4 rgba;
-void main(){
-	rgba=texture2D(utex,vtex)+vrgba*dot(vec4(1,0,0,1),vec4(vnorm,1));
-}
--- _archived_/shader_zbuf.v
+++
@@ -1,7 +0,0 @@
-#version 130
-uniform mat4 umtx_mw;
-in vec3 apos;
-void main(){
-	gl_Position=umtx_mw*vec4(apos,1);
-}
-
