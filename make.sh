bdbadb5194606257dedde637f6c57f54946a3eb5
calint <calin.tenitchi@gmail.com>
2023-12-25 04:14:56 +0100

    .

--- make.sh
+++ make.sh
@@ -7,8 +7,8 @@ SRC="src/main.cpp"
 BIN="glos"
 CFLAGS="-fno-rtti $(sdl2-config --cflags)"
 LDFLAGS=
-#LDFLAGS="-fsanitize=address -fsanitize-address-use-after-scope"
-LIBS="-lGL -lSDL2_image -lSDL2_ttf -lSDL2_mixer $(sdl2-config --libs)"
+LDFLAGS="-fsanitize=address -fsanitize-address-use-after-scope"
+LIBS="-lGL -lSDL2_image -lSDL2_ttf -lSDL2_mixer $(sdl2-config --libs) -ltbb"
 WARNINGS="-Wall -Wextra -Wpedantic -Wfatal-errors \
     -Wconversion -Wsign-conversion \
     -Wno-unsafe-buffer-usage \
@@ -19,7 +19,7 @@ REMARKS=
 PROFILE=
 PROFILE="-pg"
 
-CMD="$CC -o $BIN $SRC $PROFILE $REMARKS $OPTIMIZATION $CFLAGS $LDFLAGS $LIBS $WARNINGS -ltbb"
+CMD="$CC -o $BIN $SRC $PROFILE $REMARKS $OPTIMIZATION $CFLAGS $LDFLAGS $WARNINGS $LIBS"
 echo $CMD
 $CMD
 echo
