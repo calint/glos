36144402344740b35a61447c0f6606c72433b6ea
calint <calin.tenitchi@gmail.com>
2023-12-27 14:59:04 +0100

    .

--- make.sh
+++ make.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+# dependencies (ubuntu packages):
+# * libglm-dev/mantic,mantic,now 0.9.9.8+ds-7 all: opengl math
+# * libtbb-dev/mantic 2021.9.0-2ubuntu1 i386: for parallel unsequenced `for_each`
+# * libsdl2-dev/mantic,now 2.28.3+dfsg-2 amd64
+# * libsdl2-gfx-dev/mantic,now 1.0.4+dfsg-5 amd64
+# * libsdl2-image-dev/mantic,now 2.6.3+dfsg-2 amd64
+# * libsdl2-ttf-dev/mantic,now 2.20.2+dfsg-1 amd64
+# * opengl related packages
+
 set -e
 
 #CC="g++ -std=c++20 -Wno-changes-meaning"
