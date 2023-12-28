7dbb29b849ea43179dd133b3b16012d4b993bfc8
calint <calin.tenitchi@gmail.com>
2023-12-28 18:09:53 +0100

    .

--- src/app/objects/santa.hpp
+++ src/app/objects/santa.hpp
@@ -24,7 +24,27 @@ public:
 
     if (physics.position.y < volume.radius) {
       physics_nxt.position.y = volume.radius;
-      physics_nxt.velocity = {0, 0, 0};
+      physics_nxt.velocity.y = 0;
+    }
+
+    if (physics.position.x < -80) {
+      physics_nxt.position.x = -80;
+      physics_nxt.velocity.x = -physics_nxt.velocity.x;
+    }
+
+    if (physics.position.x > 80) {
+      physics_nxt.position.x = 80;
+      physics_nxt.velocity.x = -physics_nxt.velocity.x;
+    }
+
+    if (physics.position.z < -80) {
+      physics_nxt.position.z = -80;
+      physics_nxt.velocity.z = -physics_nxt.velocity.z;
+    }
+
+    if (physics.position.z > 80) {
+      physics_nxt.position.z = 80;
+      physics_nxt.velocity.z = -physics_nxt.velocity.z;
     }
 
     if (!net_state) {
@@ -64,8 +84,8 @@ public:
 
   inline auto on_collision(object *o, const frame_ctx &fc) -> bool override {
     assert(not is_dead());
-    printf("%u: %s collision with %s\n", fc.tick, name.c_str(),
-           o->name.c_str());
+    // printf("%u: %s collision with %s\n", fc.tick, name.c_str(),
+    //        o->name.c_str());
     return false;
   }
 };
