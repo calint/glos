586927bbcf3e0ab49dd83b4a01a4cf3e50133ad0
calint <calin.tenitchi@gmail.com>
2023-12-28 12:49:37 +0100

    .

--- src/sdl/sdl.hpp
+++ src/sdl/sdl.hpp
@@ -2,45 +2,35 @@
 // reviewed: 2023-12-22
 // reviewed: 2023-12-27
 
+#include <GLES3/gl3.h>
 #include <SDL.h>
 #include <SDL_image.h>
-#include <SDL_mixer.h>
-#include <GLES3/gl3.h>
 
 class sdl final {
-  Mix_Music *music = nullptr;
+  // Mix_Music *music = nullptr;
 
 public:
   inline void init() {
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) {
-      printf("%s %d: %s", __FILE__, __LINE__, SDL_GetError());
-      std::abort();
-    }
-
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+    if (SDL_Init(SDL_INIT_VIDEO)) {
       printf("%s %d: %s", __FILE__, __LINE__, SDL_GetError());
       std::abort();
     }
   }
 
-  inline void free() {
-    Mix_FreeMusic(music);
-    Mix_CloseAudio();
-    SDL_Quit();
-  }
+  inline void free() { SDL_Quit(); }
 
-  inline void play_mp3_path(const char *path) {
-    music = Mix_LoadMUS(path);
-    if (music == nullptr) {
-      printf("%s %d: %s", __FILE__, __LINE__, Mix_GetError());
-      std::abort();
-    }
+  // inline void play_mp3_path(const char *path) {
+  //   music = Mix_LoadMUS(path);
+  //   if (music == nullptr) {
+  //     printf("%s %d: %s", __FILE__, __LINE__, Mix_GetError());
+  //     std::abort();
+  //   }
 
-    if (Mix_PlayMusic(music, -1) == -1) {
-      printf("%s %d: %s", __FILE__, __LINE__, Mix_GetError());
-      std::abort();
-    }
-  }
+  //   if (Mix_PlayMusic(music, -1) == -1) {
+  //     printf("%s %d: %s", __FILE__, __LINE__, Mix_GetError());
+  //     std::abort();
+  //   }
+  // }
 };
 
 static sdl sdl{};
