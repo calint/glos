754b761495c3c52f295a6eefc14e87b125f187ae
calint <calin.tenitchi@gmail.com>
2023-12-31 19:00:04 +0100

    .

--- src/obj/node.hpp
+++ src/obj/node.hpp
@@ -4,7 +4,6 @@ namespace glos {
 class node final {
 public:
   glm::mat4 Mmw{}; // model -> world matrix
-  glm::mat3 Nmw{}; // model -> world normals rotation matrix
   int glo_ix = 0;
 };
 } // namespace glos
