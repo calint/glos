d529a5b740059d5a73abb951820145d3883ded2b
calint <calin.tenitchi@gmail.com>
2023-12-31 19:20:41 +0100

    .

--- src/app/application.hpp
+++ src/app/application.hpp
@@ -73,23 +73,24 @@ public:
 
     ambient_light = glm::normalize(glm::vec3{0, 1, 0});
 
-    glos::camera.type = glos::LOOK_AT;
-    glos::camera.position = {0, 50, 50};
-    glos::camera.look_at = {0, 0, 0};
+    // glos::camera.type = glos::camera::type::LOOK_AT;
+    // glos::camera.position = {0, 50, 50};
+    // glos::camera.look_at = {0, 0, 0};
 
-    // camera.type = ORTHO;
-    // camera.position = {0, 50, 0};
-    // camera.look_at = {0, 0, -0.0001f};
+    glos::camera.type = glos::camera::type::ORTHO;
+    glos::camera.position = {0, 50, 0};
+    glos::camera.look_at = {0, 0, -0.0001f};
     // note. -0.0001f because of the math of 'look at'
+    glos::camera.ortho_min_x = -10;
+    glos::camera.ortho_min_y = -10;
+    glos::camera.ortho_max_x = 10;
+    glos::camera.ortho_max_y = 10;
+
     // fit the 'grid' size
     // camera.ortho_min_x = -80;
     // camera.ortho_min_y = -80;
     // camera.ortho_max_x = 80;
     // camera.ortho_max_y = 80;
-    // camera.ortho_min_x = -10;
-    // camera.ortho_min_y = -10;
-    // camera.ortho_max_x = 10;
-    // camera.ortho_max_y = 10;
 
     // for (float y = -80; y <= 80; y += 1) {
     //   for (float x = -80; x <= 80; x += 1) {
--- src/gfx/camera.hpp
+++ src/gfx/camera.hpp
@@ -2,10 +2,10 @@
 // reviewed: 2023-12-22
 
 namespace glos {
-enum camera_type { LOOK_AT, ANGLE, ORTHO };
 
 class camera final {
 public:
+  enum class type { LOOK_AT, ANGLE, ORTHO };
   glm::vec3 position{0, 100, 100};
   glm::vec3 look_at{0, 0, 0};
   glm::vec3 angle{0, 0, 0};
@@ -18,7 +18,7 @@ public:
   float height = 1024;
   // view-projection matrix
   glm::mat4 Mvp{};
-  camera_type type = ORTHO;
+  type type = type::ORTHO;
   float ortho_min_x = -50;
   float ortho_max_x = 50;
   float ortho_min_y = -50;
@@ -30,7 +30,7 @@ public:
 
   inline void update_matrix_wvp() {
     switch (type) {
-    case LOOK_AT: {
+    case type::LOOK_AT: {
       const float aspect_ratio = height == 0 ? 1 : (width / height);
       glm::mat4 Mv = glm::lookAt(position, look_at, {0, 1, 0});
       glm::mat4 Mp = glm::perspective(glm::radians(fov), aspect_ratio,
@@ -38,7 +38,7 @@ public:
       Mvp = Mp * Mv;
       break;
     }
-    case ORTHO: {
+    case type::ORTHO: {
       glm::mat4 Mv = glm::lookAt(position, look_at, {0, 1, 0});
       // glm::mat4 Mv = glm::eulerAngleXYZ(angle.x, angle.y, angle.z);
       glm::mat4 Mp = glm::ortho(ortho_min_x, ortho_max_x, ortho_min_y,
@@ -46,7 +46,7 @@ public:
       Mvp = Mp * Mv;
       break;
     }
-    case ANGLE: {
+    case type::ANGLE: {
       const float aspect_ratio = height == 0 ? 1 : (width / height);
       glm::mat4 Mv = glm::eulerAngleXYZ(angle.x, angle.y, angle.z);
       glm::mat4 Mp = glm::perspective(glm::radians(fov), aspect_ratio,
