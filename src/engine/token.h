a05d02fef15cffba38c2b13d8f8261110fece365
calint <calin.tenitchi@gmail.com>
2024-01-08 19:27:41 +0100

    .

--- src/engine/token.h
+++ src/engine/token.h
@@ -10,22 +10,19 @@ typedef struct token {
 #define token_def                                                              \
   (token) { NULL, NULL, NULL, NULL }
 
-static inline unsigned token_size(token const *t) {
+static inline auto token_size(token const *t) -> unsigned {
   return (unsigned)(t->content_end - t->content);
 }
 
-static inline bool token_starts_with(token const *t, char const *str) {
+static inline auto token_starts_with(token const *t, char const *str) -> bool {
   return strncmp(str, t->content, strlen(str)) == 0;
 }
 
-static inline bool token_equals(token const *t, char const *str) {
+static inline auto token_equals(token const *t, char const *str) -> bool {
   char const *p = t->content;
   while (true) {
     if (p == t->content_end) {
-      if (!*str) {
-        return true;
-      }
-      return false;
+      return not *str;
     }
     if (*p != *str) {
       return false;
@@ -35,16 +32,17 @@ static inline bool token_equals(token const *t, char const *str) {
   }
 }
 
-static inline float token_get_float(token const *t) {
-  return (float)atof(t->content); //? assuming file ends with whitespace
+static inline auto token_get_float(token const *t) -> float {
+  return float(atof(t->content)); //? assuming file ends with whitespace
 }
 
-static inline int token_get_int(token const *t) {
-  return atoi(t->content); //?  assuming file ends with whitespace
+static inline auto token_get_int(token const *t) -> float {
+  return float(atoi(t->content)); //?  assuming file ends with whitespace
 }
 
-static inline unsigned token_get_uint(token const *t) {
-  int i = atoi(t->content); //?  assuming file ends with whitespace, error?
+static inline auto token_get_uint(token const *t) -> unsigned {
+  int const i = atoi(t->content);
+  //?  assuming file ends with whitespace, error?
   if (i < 0) {
     fprintf(stderr, "\n%s:%d: unexpected int\n", __FILE__, __LINE__);
     fflush(stderr);
@@ -53,77 +51,52 @@ static inline unsigned token_get_uint(token const *t) {
   return (unsigned)i;
 }
 
-static inline token token_next(char const **s) {
+static inline auto token_next(char const **s) -> token {
   const char *p = *s;
   token t;
   t.begin = p;
-  while (1) {
-    if (!*p)
+  while (true) {
+    if (not *p or not isspace(*p)) {
       break;
-    if (!isspace(*p))
-      break;
+    }
     ++p;
   }
   t.content = p;
-  while (1) {
-    if (!*p)
+  while (true) {
+    if (not *p or isspace(*p) or *p == ':' or *p == '{' or *p == '}' or
+        *p == ',' or *p == '=' or *p == ';' or *p == '(' or *p == ')' or
+        *p == '!' or *p == '>' or *p == '<') {
       break;
-    if (isspace(*p))
-      break;
-    if (*p == ':')
-      break;
-    if (*p == '{')
-      break;
-    if (*p == '}')
-      break;
-    if (*p == ',')
-      break;
-    if (*p == '=')
-      break;
-    if (*p == ';')
-      break;
-    if (*p == '(')
-      break;
-    if (*p == ')')
-      break;
-    if (*p == '!')
-      break;
-    if (*p == '>')
-      break;
-    if (*p == '<')
-      break;
+    }
     ++p;
   }
   t.content_end = p;
-  while (1) {
-    if (!*p)
+  while (true) {
+    if (not *p or not isspace(*p)) {
       break;
-    if (!isspace(*p))
-      break;
+    }
     ++p;
   }
   *s = t.end = p;
   return t;
 }
 
-static inline token token_from_string_additional_delim(char const *s,
-                                                       char delim) {
+static inline auto token_from_string_additional_delim(char const *s, char delim)
+    -> token {
   const char *p = s;
   token t;
   t.begin = s;
-  while (1) {
-    if (!*p)
+  while (true) {
+    if (not *p or not isspace(*p)) {
       break;
-    if (!isspace(*p))
-      break;
+    }
     ++p;
   }
   t.content = p;
-  while (1) {
-    if (!*p)
+  while (true) {
+    if (not *p or isspace(*p)) {
       break;
-    if (isspace(*p))
-      break;
+    }
     if (*p == delim) {
       ++p;
       t.end = t.content_end = p;
@@ -132,21 +105,21 @@ static inline token token_from_string_additional_delim(char const *s,
     ++p;
   }
   t.content_end = p;
-  while (1) {
-    if (!*p)
+  while (true) {
+    if (not *p or not isspace(*p)) {
       break;
-    if (!isspace(*p))
-      break;
+    }
     ++p;
   }
   t.end = p;
   return t;
 }
 
-static inline char const *scan_to_including_newline(char const *p) {
-  while (1) {
-    if (!*p)
+static inline auto scan_to_including_newline(char const *p) -> char const * {
+  while (true) {
+    if (not *p) {
       return p;
+    }
     if (*p == '\n') {
       ++p;
       return p;
