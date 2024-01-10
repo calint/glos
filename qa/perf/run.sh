39146af6202e305d6373f8559ef349a9f5a8846f
calint <calin.tenitchi@gmail.com>
2024-01-10 18:12:43 +0100

    .

--- qa/perf/run.sh
+++ qa/perf/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo perf record -B -e cache-references,cache-misses,branches,branch-misses,cycles,instructions,faults,migrations ./glos 
-sudo perf report
+perf record -B -e cache-references,cache-misses,branches,branch-misses,cycles,instructions,faults,migrations ./glos 
+perf report
