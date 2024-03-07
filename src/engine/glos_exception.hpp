c8da22c14a78a05e5da468b7703e4955d39bb66e
calint <calin.tenitchi@gmail.com>
2024-03-07 19:37:48 +0100

    .

--- src/engine/glos_exception.hpp
+++ src/engine/glos_exception.hpp
@@ -4,11 +4,11 @@ class glos_exception : public std::exception {
 public:
   std::string message{};
 
-  glos_exception(std::string const &msg, std::source_location const &src_loc =
-                                             std::source_location::current())
-      : message{std::format("{}:{} - {}", src_loc.file_name(), src_loc.line(),
-                            src_loc.function_name()) +
-                ": " + msg} {}
+  inline glos_exception(
+      std::string const &msg,
+      std::source_location const &src_loc = std::source_location::current())
+      : message{std::format("{}:{} - {}: {}", src_loc.file_name(),
+                            src_loc.line(), src_loc.function_name(), msg)} {}
 
-  char const *what() const noexcept override { return message.c_str(); }
+  inline char const *what() const noexcept override { return message.c_str(); }
 };
